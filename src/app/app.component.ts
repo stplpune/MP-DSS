63290cae02ead1d80ebc7c53de9dff960840b396
OmkarDivekar-ShauryaTech <omkar.divekar@shauryatechnosoft.com>
2023-11-03 16:41:59 +0530

    sidebar component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layouts/public/header/header.component';
+import { FooterComponent } from './layouts/public/footer/footer.component';
+import { SidebarComponent } from './layouts/secure/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent,FooterComponent,SidebarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
