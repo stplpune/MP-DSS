9811c7777d1018843df01531a6b53a2bdf60b3c3
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2023-11-06 14:20:29 +0530

    header page designed

--- src/app/layouts/secure/header/header.component.ts
+++ src/app/layouts/secure/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
--- src/app/layouts/secure/secure.route.ts
+++ src/app/layouts/secure/secure.route.ts
@@ -1,6 +1,7 @@
 import { Route } from '@angular/router';
 
 export default [
+    { path: 'dashboard', loadComponent: () => import('../../components/dashboard/dashboard.component').then(c => c.DashboardComponent) },
     { path: 'ai-toll-verification', loadComponent: () => import('../../components/ai-toll-verification/ai-toll-verification.component').then(c => c.AiTollVerificationComponent) },
     { path: 'ai-toll-vehicleUpdate', loadComponent: () => import('../../components/ai-toll-vehicle-update/ai-toll-vehicle-update.component').then(c => c.AiTollVehicleUpdateComponent) },
 ] as Route[];
