d0ad0cb99708d2f2a05ee70220bea58bbea17dbf
yogesh7355 <yogesh.jadhav@shauryatechnosoft.com>
2023-11-07 12:39:49 +0530

    user right access component creation and functionality

--- src/app/layouts/secure/secure.route.ts
+++ src/app/layouts/secure/secure.route.ts
@@ -4,4 +4,5 @@ export default [
     { path: 'Dashboard', loadComponent: () => import('../../components/dashboard/dashboard.component').then(c => c.DashboardComponent) },
     { path: 'AI-Toll-Verification', loadComponent: () => import('../../components/ai-toll-verification/ai-toll-verification.component').then(c => c.AiTollVerificationComponent) },
     { path: 'AI-Toll-VehicleUpdate', loadComponent: () => import('../../components/ai-toll-vehicle-update/ai-toll-vehicle-update.component').then(c => c.AiTollVehicleUpdateComponent) },
+    { path: 'User-Access', loadComponent: ()=> import ('../../components/user-access/user-access.component').then(c => c.UserAccessComponent) },
 ] as Route[];
