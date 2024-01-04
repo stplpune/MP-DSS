784e4336fc228a6c5b3b02b1b5968c80ed593b85
stpl-somnath <somnath.khabale@shauryatechnosoft.com>
2024-01-04 12:19:14 +0530

    Remove all files

--- src/app/layouts/public/public.route.ts
+++
@@ -1,9 +0,0 @@
-import { Route } from '@angular/router';
-
-export default [
-    {path:'',redirectTo:'login',pathMatch:'full'},
-    { path: 'login', loadComponent: () => import('../../components/login/login.component').then(c => c.LoginComponent), data: { breadcrumb: [{ title: 'Login', active: true }] }},
-    { path: 'home', loadComponent: () => import('../../components/home/home.component').then(c => c.HomeComponent) , data: { breadcrumb: [{ title: 'Home', active: true }] }},
-] as Route[];
-
-
--- src/app/layouts/secure/secure.route.ts
+++
@@ -1,8 +0,0 @@
-import { Route } from '@angular/router';
-
-export default [
-    { path: 'Dashboard', loadComponent: () => import('../../components/dashboard/dashboard.component').then(c => c.DashboardComponent) , data: { breadcrumb: [{ title: 'Dashboard', active: true }] }},
-    { path: 'AI-Toll-Verification', loadComponent: () => import('../../components/ai-toll-verification/ai-toll-verification.component').then(c => c.AiTollVerificationComponent) , data: { breadcrumb: [{ title: 'AI Toll Verification', active: true }] }},
-    { path: 'AI-Toll-VehicleUpdate', loadComponent: () => import('../../components/ai-toll-vehicle-update/ai-toll-vehicle-update.component').then(c => c.AiTollVehicleUpdateComponent) , data: { breadcrumb: [{ title: 'AI Toll Vehicle Update', active: true }] }},
-    { path: 'page-right-access', loadComponent: ()=> import ('../../components/user-access/user-access.component').then(c => c.UserAccessComponent) , data: { breadcrumb: [{ title: 'Page Access', active: true }] }},
-] as Route[];
